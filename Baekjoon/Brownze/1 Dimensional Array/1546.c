1f08176a1680788f6c595c5eb8bd5b52f7286d34
Jonghyeon Kim <85392646+ryankimjh00@users.noreply.github.com>
2022-02-16 17:19:39 +0900

    Update 1546.c

--- Baekjoon/Brownze/1 Dimensional Array/1546.c
+++ Baekjoon/Brownze/1 Dimensional Array/1546.c
@@ -2,15 +2,15 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 int main() {
-	int n, score, big = 0;
+	int n, score, max = 0;
 	double sum = 0;
 	scanf_s("%d", &n);
 	for (int i = 0; i < n; i++) {
 		scanf_s("%d", &score);
 		sum += score;
-		if (big < score)
-			big = score;
+		if (max < score)
+			max = score;
 	}
-	printf("%.2lf", (sum / big * 100) / n);
+	printf("%.2lf", (sum / max * 100) / n);
 	return 0;
 }
