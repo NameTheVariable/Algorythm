fbeb24cd091109310148fc7d16d885a5512c83dd
Jonghyeon Kim <85392646+ryankimjh00@users.noreply.github.com>
2022-08-18 23:30:53 +0900

    다음 큰 숫자, 주식가격

--- Programmers/LV.2/다음 큰 숫자.py
+++ Programmers/LV.2/다음 큰 숫자.py
@@ -1,3 +1,13 @@
+def count_bin_one(i):  # binary로 변환한 숫자의 1의 갯수를 세는 함수
+    binarynumber = bin(i)
+    count_one = binarynumber.count('1')
+    return count_one
+
+
 def solution(n):
-    answer = 0
-    return answer
+    answer = n
+    num = count_bin_one(n)
+    while True:
+        answer += 1
+        if int(count_bin_one(answer)) == num:
+            return answer
--- Programmers/LV.2/주식가격.py
+++ Programmers/LV.2/주식가격.py
@@ -1,3 +1,12 @@
 def solution(prices):
-    answer = []
+
+    answer = [0] * len(prices)  # 일단 index에 값이 있어야함
+
+    for i in range(len(prices)):
+        for j in range(i + 1, len(prices)):
+            if prices[i] <= prices[j]:
+                answer[i] += 1
+            else:
+                answer[i] += 1
+                break
     return answer
