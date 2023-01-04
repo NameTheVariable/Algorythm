c4401b579129f2bdd52efee5af7f0beeaa0b962b
ryankimjh00 <ryankimjh546@gmail.com>
2023-01-04 19:19:54 +0900

    ...

---
+++ LeetCode/Medium/Number of Islands.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        def dfs(i, j):
+            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] != '1':
+                return
+            grid[i][j] = '0'
+
+            dx = [0, 0, -1, 1]
+            dy = [-1, 1, 0, 0]
+            for x in range(4):
+                dfs(i + dx[x], j + dy[x])
+
+        count = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    dfs(i, j)
+                    count += 1
+        return count
