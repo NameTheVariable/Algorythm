3968c56c617229b5ac377c67890bbaea254c0d4c
ryankimjh00 <ryankimjh546@gmail.com>
2023-02-20 22:21:14 +0900

    SpiralMatrix2 done

--- LeetCode/Medium/Spiral Matrix II.py
+++ LeetCode/Medium/Spiral Matrix II.py
@@ -1,33 +1,24 @@
 from typing import List
 class Solution:
-    def generateMatrix(self, n) -> List[List[int]]:
-        res = [[0]* n for _ in range(n)]
-        col, row, num_elements, e, zone, data = n, n, n*n, 0, 0, 1
-        
-        while data <= num_elements:
-            # move right 
-            for i in range(zone, col - zone):
-                res[zone][i] = data
-                data +=1
-            if data == num_elements+1:
-                return res
-            # move down
-            for j in range(zone+1,row-zone):
-                res[j][col-1-zone] = data
-                data +=1
-            if data == num_elements+1:
-                return res
-            # move left
-            for i in range(col-1-zone,zone,-1):
-                res[col-1-zone][i-1] = data
-                data+=1
-            if data == num_elements+1:
-                return res
-            # move up
-            for j in range(row-2-zone,zone,-1):
-                res[j][zone] = data
-                data+=1
-            if data == num_elements+1:
-                return res
-            zone +=1
-        return res     
+    def generateMatrix(self, n: int) -> List[List[int]]:
+        matrix = [[0]* n for _ in range(n)]
+        x_start, x_end, y_start, y_end, num = 0, n - 1, 0, n - 1, 1
+
+        while x_start <= x_end and y_start <= y_end:
+            for x in range(x_start, x_end + 1): # move right
+                matrix[y_start][x] = num
+                num += 1
+            for y in range(y_start + 1, y_end): # move down
+                matrix[y][x_end] = num
+                num += 1
+            for x in reversed(range(x_start, x_end + 1)): # move left
+                if y_start < y_end:
+                    matrix[y_end][x] = num
+                    num += 1
+            for y in reversed(range(y_start + 1, y_end)): # move up
+                if x_start < x_end:
+                    matrix[y][x_start] = num
+                    num += 1
+            x_start, x_end, y_start, y_end = x_start + 1, x_end - 1, y_start + 1, y_end - 1
+
+        return matrix
